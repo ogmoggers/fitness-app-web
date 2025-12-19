9b96c0aa68c1cdaed37860fd1fbb099bb6f2a96e
KOKENOVE <erasyl.kokenov@bcchub.kz>
2025-12-19 08:51:41 +0500

    fix(hardcoding): supabase hardcode

--- app/shared/utils/supabase.ts
+++ app/shared/utils/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NUXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NUXT_PUBLIC_SUPABASE_KEY;
+// const supabaseUrl = process.env.NUXT_PUBLIC_SUPABASE_URL;
+// const supabaseKey = process.env.NUXT_PUBLIC_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = "https://zdkknlnbalcjaxsqxbvn.supabase.co"; 
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpka2tubG5iYWxjamF4c3F4YnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNjk1ODgsImV4cCI6MjA4MTY0NTU4OH0.tkM07_SB6EwHEWSFH_L3UdhvPxiov_ns9vMFN2JKud8"
+
+export const supabase = createClient(supabaseUrl!, supabaseKey!);
